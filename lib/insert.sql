b3166f90e787fcdc9d84ebe18fea4e093be915b8
Vicki Hamm <vicki.cello@gmail.com>
2018-08-05 19:43:13 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,1 @@
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) 
-VALUES ('Zoom', 'Software', 5000, 2/4/2018, 10/4/2018);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ('Zoom', 'Software', 5000, '2/4/2018', '10/4/2018');
